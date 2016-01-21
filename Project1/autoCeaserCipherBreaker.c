1f35992c57328cc9bbfb5936c57eaf08a012a207
Justin Rohr <rohrj@mail.gvsu.edu>
2016-01-21 15:51:24 -0500

    Rotate function complete and tested

--- Project1/autoCeaserCipherBreaker.c
+++ Project1/autoCeaserCipherBreaker.c
@@ -3,19 +3,29 @@
  *
  * Description: The program is an automatic Ceaser cipher breaker that uses a Letter Frequency file to then crack the code for Ceaser ciphers. Input is a encrypted file to be cracked and to display the decoded text once done.
  */
-include <stdio.h>
-include <stdlib.h>
-include <ctype.h>
-include <autoCeaserCipherBreaker.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+//#include <autoCeaserCipherBreaker.h>
+char rotate(char ch, int num);
 int main (){
-
+	char z = rotate('a', 25);
+	char a = rotate('z', 1);
+	char Z = rotate('A', 25);
+	char A = rotate('Z', 1);
+	printf("z: %c   a: %c  Z: %c   A: %c  \n", z, a, Z, A);
 	return EXIT_SUCCESS;
 }
 
 // Load array given with the letter frequencies for English from file LetFreq.txt
 void readFreq(float given[], char fname[]){
-
+	char ch;
+	float num;
+	int position = 0;
+	while (fscanf(stdin, "%c ", &ch) != EOF){
+		fscanf(stdin, "%d", &num);
+		given[position] = num;
+		position++;
+	}
 }
 
 // Read the encoded text from an input file and accumulate the letter frequency
@@ -27,7 +37,10 @@ void calcFreq(float found[], char fname[]){
 // Rotate the character in parameter ch down the alphabet for the number of
 // positions as given in parameter num and return the resulting character.
 char rotate(char ch, int num){
-
+	int final = ch + num;
+	if ((final > 90 && ch < 91) || final > 122)
+		final -= 26;
+	return final;
 }
 
 // Compare the data in array found with the frequency data in array given, looking
@@ -35,11 +48,13 @@ char rotate(char ch, int num){
 // and remember which gives the smallest difference between the frequencies you
 // observed and the frequencies given. Return the key.
 int findKey(float given[], float found[]){
-
+	int key;
+	
+	return key;
 }
 
 // Decrypt the encoded text in the input file using the key and display the decoded text
 void decrypt(int key){
-
+	//Read file and use rotate(key) on each char to get the message
 }
 
